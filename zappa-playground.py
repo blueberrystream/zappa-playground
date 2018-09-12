65f95ae8f6ba9bf97d4efd6f2f50e09e405f9080
KID the Euforia <kid0725+github@gmail.com>
2018-09-12 16:41:33 +0900

    use app.logger instead of logger
    Signed-off-by: KID the Euforia <kid0725+github@gmail.com>

--- zappa-playground.py
+++ zappa-playground.py
@@ -1,15 +1,10 @@
-import logging
 from flask import Flask
 
 app = Flask(__name__)
 
-logging.basicConfig()
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 
 @app.route('/', methods=['GET', 'POST'])
 def index(event=None, context=None):
-    logger.info('Lambda function invoked index()')
     return 'hello from Flask!'
 
 
