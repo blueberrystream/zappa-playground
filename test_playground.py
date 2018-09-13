6df7a1d12101f2821ef777a0a1b2f95c9b629652
KID the Euforia <kid0725+github@gmail.com>
2018-09-13 18:42:02 +0900

    unit testとCircleCI設定を追加
    Signed-off-by: KID the Euforia <kid0725+github@gmail.com>

---
+++ test_playground.py
@@ -0,0 +1,13 @@
+import pytest
+
+import playground
+
+@pytest.fixture
+def client():
+    app = playground.create_app()
+    client = app.test_client()
+    yield client
+
+def test_index(client):
+    rv = client.get('/')
+    assert b'hello from Flask!' in rv.data
