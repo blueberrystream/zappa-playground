5bd5fbadd0e4b2b7770f3fa421d9dab4dc411cca
KID the Euforia <kid0725+github@gmail.com>
2018-09-21 18:24:53 +0900

    set region in Log's meta
    Signed-off-by: KID the Euforia <kid0725+github@gmail.com>

--- models/log.py
+++ models/log.py
@@ -4,6 +4,7 @@ from pynamodb.attributes import UnicodeAttribute, UTCDateTimeAttribute
 class Log(Model):
     class Meta:
         table_name = 'zappa-playground-logs'
+        region = 'ap-northeast-1'
 
     name = UnicodeAttribute(hash_key=True)
     ip_address = UnicodeAttribute()
