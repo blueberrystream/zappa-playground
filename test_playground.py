051f858a715ea7d748d0f901f229bf5362643c0e
KID the Euforia <kid0725+github@gmail.com>
2018-09-25 12:58:37 +0900

    fix flake8 errors
    Signed-off-by: KID the Euforia <kid0725+github@gmail.com>

--- test_playground.py
+++ test_playground.py
@@ -2,11 +2,13 @@ import pytest
 
 import playground
 
+
 @pytest.fixture
 def client():
     app = playground.create_app()
     client = app.test_client()
     yield client
+
 
 def test_index(client):
     rv = client.get('/')
